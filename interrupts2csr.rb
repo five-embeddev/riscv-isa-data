c9cedac7165f851c58857385a0db356a85b72311
Phil <pmulholland@shincbm.com>
2022-05-07 23:30:46 +0930

    Add exceptions to interrupts.yaml. Expand platform_defined interrupts, mark as custom.

--- interrupts2csr.rb
+++ interrupts2csr.rb
@@ -23,10 +23,49 @@ DESC = {
 
 %w{m s u}.each do | level |
 
+  %w{cause}.each do | reg |
+    
+    csr_name = "#{level}#{reg}"
+    if csrs["regs"].include?csr_name
+      
+      interrupts["exceptions"].each do | exception |
+        if exception["modes"].include?level
+          
+          if not csrs["regs"][csr_name]["fields"]["exception_code"].include?("enums")
+            csrs["regs"][csr_name]["fields"]["exception_code"]["enums"] = {}
+          end    
+          
+          csrs["regs"][csr_name]["fields"]["exception_code"]["enums"][exception["macro"]] = exception["value"]
 
+        end
+        
+      end
+    end
+  end
+
+  %w{cause}.each do | reg |
+    
+    csr_name = "#{level}#{reg}"
+    if csrs["regs"].include?csr_name
+
+      if not csrs["regs"][csr_name]["fields"]["interrupt_code"].include?("enums")
+        csrs["regs"][csr_name]["fields"]["interrupt_code"]["enums"] = {}
+      end    
+
+      interrupts["interrupts"].each do | interrupt |
+        interrupt_name = interrupt["name"]
+        if check_level(level, interrupt_name[0])
+          csrs["regs"][csr_name]["fields"]["interrupt_code"]["enums"][interrupt_name] = interrupt["idx"]
+        end
+
+      end
+
+    end
+
+  end
   
   %w{ie ip}.each do | reg |
-    
+
     interrupts["interrupts"].each do | interrupt |
 
       csr_name = "#{level}#{reg}"
@@ -35,6 +74,11 @@ DESC = {
       if check_level(level, interrupt_name[0])
 
         if csrs["regs"].include?csr_name
+          
+          custom=FALSE
+          if interrupt.include?('custom')
+            custom = interrupt['custom'] 
+          end
           
           csrs["regs"][csr_name]["fields"] ||= {}
           if not csrs["regs"][csr_name]["fields"].include?(interrupt_name)
@@ -48,9 +92,13 @@ DESC = {
             field_info = {
               "desc" => "#{DESC[reg]}: #{interrupt['desc']}",
               "bits" => bits,
+              "custom" => custom,
             }
             csrs["regs"][csr_name]["fields"][interrupt_name] = field_info
             print "#{csr_name}.#{interrupt_name} = #{field_info.to_yaml}\n"
+            
+          else
+            csrs["regs"][csr_name]["fields"][interrupt_name]['custom'] = custom
             
           end
 
